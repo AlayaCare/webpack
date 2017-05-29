f6d6ed4586e5a480d6585256f8595868fdbbfce2
luis doebbel <luis.doebbel@alayacare.com>
2017-05-29 09:45:58 -0400

    loader fix

--- template/build/vue-loader.conf.js
+++ template/build/vue-loader.conf.js
@@ -4,13 +4,14 @@ var isProduction = process.env.NODE_ENV === 'production'
 var isForDistribution = process.env.FOR_DISTRIBUTION === 'true'
 
 module.exports = {
-  loaders: utils.cssLoaders({
-    sourceMap: isProduction
-      ? config.build.productionSourceMap
-      : config.dev.cssSourceMap,
+  loaders: Object.assign(
+    {},
+    utils.cssLoaders({
+      sourceMap: isProduction
+        ? config.build.productionSourceMap
+        : config.dev.cssSourceMap,
       extract: isProduction && !isForDistribution
-  }),
-  {
-    i18n: '@kazupon/vue-i18n-loader'
-  }
+    }),
+    { i18n: '@kazupon/vue-i18n-loader' }
+  ),
 }
