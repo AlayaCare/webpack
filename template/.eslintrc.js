29e3c5c2059d6cc93733bfdd64b087fbe833ec09
luis doebbel <luis.doebbel@alayacare.com>
2017-08-11 15:22:21 -0400

    added lint --fix flag to script

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
   },
   env: {
     browser: true,
+    mocha: true
   },
   {{#if_eq lintConfig "standard"}}
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
